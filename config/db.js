a14aa434a0e8714f6f312d78e4ea366bd5c8bc3f
bclaringbold <bclaringbold@gmail.com>
2015-11-17 21:58:10 -0500

    Update db connection

--- config/db.js
+++ config/db.js
@@ -1,5 +1,10 @@
 module.exports = {
 
-'url':'mongodb://localhost/userDB'
+//connection to localDB
+//'url':'mongodb://localhost/userDB'
+
+//live connection on MongoLab
+'url':'mongodb://portfolio-user:P0rtf0l10@ds042128.mongolab.com:42128/portfolio'
+
 
 };
